1b5bc1f52a82e03e796e0d400c9212688ecc7100
KilaBash <yefancy@foxmail.com>
2025-07-17 23:02:20 +0900

    Fixed support for rendering radius and global renderer.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,5 @@
 # ChangeLog
 
-## v1.0.34
-* Fixed proxy recipes incompatible with kjs recipe inject event.
-* Added support for rendering radius and global renderer.
-* Move trait dropping to the onDrop event.
+## v1.0.34.a
+* Fixed support for rendering radius and global renderer.
 
--- src/main/java/com/lowdragmc/mbd2/common/machine/definition/config/MachineState.java
+++ src/main/java/com/lowdragmc/mbd2/common/machine/definition/config/MachineState.java
@@ -57,11 +57,11 @@ public class MachineState implements IConfigurable, IPersistedSerializable, Comp
             {"config.machine_state.rendering_box.tooltip.0", "config.machine_state.rendering_box.tooltip.1",
                     "config.machine_state.rendering_box.tooltip.2"})
     protected final ToggleAABB renderingBox;
-    @Configurable(name = "config.machine_state.is_global_visible", subConfigurable = true, tips =
+    @Configurable(name = "config.machine_state.is_global_visible", tips =
             "config.machine_state.is_global_visible.tooltip")
     @Setter
     protected boolean isGlobalVisible = false;
-    @Configurable(name = "config.machine_state.rendering_radius", subConfigurable = true, tips =
+    @Configurable(name = "config.machine_state.rendering_radius", tips =
             "config.machine_state.rendering_radius.tooltip")
     @NumberRange(range = {1, Integer.MAX_VALUE})
     @Setter
